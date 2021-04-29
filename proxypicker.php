f89fd82b1cfc63621999848e758bd972cba89bfe
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2021-04-29 07:45:42 +0700

    Update proxypicker.php

--- proxypicker.php
+++ proxypicker.php
@@ -7,15 +7,4 @@ $ex = explode("<br>",$url);
 foreach ($ex as $key) {
 echo "$key";
 }
-#Dear Tukang Recode :
-#[=============Baca Hadist ini=============]
-#Lelaki yang mencuri dan wanita yang mencuri,potonglah tangan
-#keduanya (sebagai) pembalasan bagi apa yang mereka kerjakan dan
-#sebagai siksaan dari Allah.
-#Dan Allah Maha Perkasa lagi Maha Bijaksana.
-#Maka barangsiapa bertaubat (di antara pencuri-pencuri itu)
-#sesudah melakukan kejahatan itu dan memperbaiki diri,
-#maka sesungguhnya Allah menerima taubatnya.
-#Sesungguhnya Allah Maha Pengampun lagi Maha Penyayang.”
-#(QS. Al-Maidah: 38-39)
 ?>
